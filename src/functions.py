ac214b70d19b62eb0fde8cb5aa9d231587d9efb4
AlexMndzF <alex.93menendez@gmail.com>
2019-11-02 17:46:30 +0100

    Cambios en funciones y fichero global

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,5 @@
+import re
+
 def changeCharacterSex(text):
     if text == "M ":
         return "M"
@@ -15,3 +17,19 @@ def changeFatalValue(text):
         'N ':'N'
         }
     return dic.get(text,text)
+def removefinalcharacters(text):
+    if text[-1] == " ":
+        return text[0:(len(text)-1)]
+    elif text[-1] == "?":
+         return text[0:(len(text)-1)]
+    else:
+        return text
+def ChangeType(text):
+    dic = {
+        'Invalid':'other',
+        'Sea Disaster':'other',
+        'Boat':'other',
+        'Boating':'other' 
+    } 
+    return dic.get(text,text)
+
